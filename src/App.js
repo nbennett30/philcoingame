1fd4e9db2ec215663238df7cc0b094ef29e24875
Nick <nbennett30@gatech.edu>
2021-03-14 23:13:17 -0400

    initial build

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import philcoin from './philcoin.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={philcoin} className="App-logo" alt="logo" />
+        <p>
+        ⚠️ Under Construction ⚠️
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
